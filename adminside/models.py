732ea5ffec270f92e11d06b27e331a4f7416f225
noorhana123 <hananoorudheen721@gmail.com>
2022-10-27 19:26:55 +0530

    templates added 2

--- adminside/models.py
+++ adminside/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+
+    Name = models.CharField(max_length=100, default=None)
+    RegNumber = models.IntegerField(blank=True, null=True, default=None)
+    Email = models.EmailField(max_length=100, default=None)
+    Mob = models.CharField(max_length=13, default=None)
+    Semester = models.IntegerField(default=None)
+    Photo = models.ImageField(upload_to='pics')
+
+
+

50cf21183c269dd83745ffae1c4fee3b4b2e704c
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2022-10-28 00:48:45 +0530

    added req and notification route

--- adminside/views.py
+++ adminside/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return (render(request,'Admin/index.html'))
+
+def req(request):
+    return (render(request,'Admin/req.html'))
+
+def noti(request):
+    return (render(request,'Admin/noti.html'))

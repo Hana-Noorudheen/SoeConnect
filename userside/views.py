7053489c55b04f3bd190514406f7fff9de363153
devaprasadnm <devaprasadmohan@gmail.com>
2022-10-27 22:04:54 +0530

    reg  views setup

--- userside/views.py
+++ userside/views.py
@@ -1,7 +1,18 @@
 
 from django.shortcuts import render
-
+from adminside.models import Students
 # Create your views here.
 
 def index(request):
     return (render(request,'Admin/index.html'))
+
+
+def register(request):
+    if request.method == 'POST':
+            Name = request.POST['Name']
+            Gender = request.POST['Gender']
+            Address = request.POST['Address']
+            Mob = request.POST['Mob']
+
+
+            Email = request.POST['Email']
